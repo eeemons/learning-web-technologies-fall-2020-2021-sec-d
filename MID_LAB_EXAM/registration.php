df826227c99d61ec7d56a2bc1eeb0918fc1addb4
Emon <e01716@gmail.com>
2020-11-11 09:36:37 +0600

    reg update

--- MID_LAB_EXAM/registration.php
+++ MID_LAB_EXAM/registration.php
@@ -3,7 +3,7 @@
      if(!empty($_REQUEST['name']) || !empty($_REQUEST['id']) || !empty($_REQUEST['password']) || !empty($_REQUEST['rad'])){
          if($_REQUEST['password'] == $_REQUEST['conpass']){            
              $myfile = fopen('data.txt', 'a');
-             $data = $_REQUEST['rad']."::".$_REQUEST['password']."|".$_REQUEST['id']."|".$_REQUEST['name']."\n";
+             $data = $_REQUEST['rad']."::".$_REQUEST['password']."::".$_REQUEST['id']."::".$_REQUEST['name']."\n";
              fwrite($myfile, $data);
              fclose($myfile);   
          }
