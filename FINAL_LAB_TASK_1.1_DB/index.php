c51e517ebe4b77aa03614a2466977cf31945af6d
Emon <e01716@gmail.com>
2020-11-29 09:56:11 +0600

    work in progress

---
+++ FINAL_LAB_TASK_1.1_DB/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
+<body>
+    <br><br><br><br><br><br>
+    <center>
+        <h1>Login</h1>
+        <form action="controller/login.php" method="post">
+            Username:
+            <input type="text" name="username" placeholder="Enter username"><br><br>
+            Password:
+            <input type="password" name="password"placeholder="Enter password"> <br> <br>
+            <input type="submit" name="login" value="Login">
+        </form>
+        
+    </center>
+    
+</body>
+</html>
